83b44db9d0da423b43e02dad0af1e01f3d0ef7ce
Aditya Upadhyay <34938174+aditya-u@users.noreply.github.com>
2020-09-07 18:16:02 +0530

    Add files via upload

--- data.h
+++ data.h
@@ -1,6 +1,7 @@
 #ifndef extern_
  #define extern_ extern
 #endif
-extern_ int     Line;
-extern_ int	Putback;
-extern_ FILE	*Infile;
+extern_ int Line;
+extern_ int Putback;
+extern_ FILE *Infile;
+extern_ struct token Token;
